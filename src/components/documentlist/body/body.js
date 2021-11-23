35cb9466c138a84ab732b20cae0e35eb5a53a8de
Anh Tuan <tuanstyle252@gmail.com>
2021-11-23 22:09:03 +0700

    first commit

--- src/components/documentlist/body/body.js
+++ src/components/documentlist/body/body.js
@@ -11,8 +11,18 @@ import Footer from '../../Footer/Footer';
 import './document.css'
 import Logo from '../../../assets/icons/remind.png'
 import Logo1 from '../../../assets/icons/phongkham.png'
-const bodydocument = () =>{
+import Logo2 from '../../../assets/icons/word.png'
+import Logo3 from '../../../assets/icons/view.png'
+import Logo4 from '../../../assets/icons/dow.png'
+import Logo5 from '../../../assets/icons/excel.png'
+import Logo6 from '../../../assets/icons/pdf.png'
+import excel from '../../../assets/file/test.xlsx'
+import word from '../../../assets/file/word.docx'
 
+import pdf from '../../../assets/file/word.pdf'
+
+const bodydocument = () =>{
+    
     return (
         <div className="main">
                 <Headerdocument></Headerdocument>
@@ -83,24 +93,65 @@ const bodydocument = () =>{
                    
                         <div className="box1">
                             <div className="box-header1">
-                                <label className="box-header-title">Tài Liệu Tải Lên Bởi Phòng Khám:</label>
+                                <label className="box-header-title1">Tài Liệu Tải Lên Bởi Phòng Khám:</label>
                                 </div>
                             <div className='file'>
-                                <div className='file_box'>
+                               
                                 <p href='' className="header_file">
-                                <img src={Logo} alt='' className='header_file-img'/>
+                                <img src={Logo2} alt='' className='header_file-img'/>
                                 <div className='header_file-info'>
-                                <span className='header_file-name'>FileName</span>
+                                <span className='header_file-name'>File Name</span>
                                 <span className='header_file-descriotion'>File description</span>
-                                <span className='header_file-descriotion'>File description</span>
+                                <button><span className='header_file-descriotion'>
+                                <img src={Logo3} alt='' className='header_file-img1'/>
+                                <a href={word} download='word'><span className='header_file-descriotion'>
+                                <img src={Logo4} alt='' className='header_file-img1'/>
+                                </span></a>
+                                </span>
+                                </button>
                                 </div>
+                                
 
                             </p>
-                         
+                            <p href='' className="header_file">
+                                <img src={Logo5} alt='' className='header_file-img'/>
+                                <div className='header_file-info'>
+                                <span className='header_file-name'>File Name</span>
+                                <span className='header_file-descriotion'>File description</span>
+                                <button><span className='header_file-descriotion'>
+                                <img src={Logo3} alt='' className='header_file-img1'/>
+                                <a href={excel} download='excel'><span className='header_file-descriotion'>
+                                <img src={Logo4} alt='' className='header_file-img1'/>
+                                </span></a>
+                                </span>
+                                </button>
+                                </div>
                                 
+
+                            </p>
+                            <p href='' className="header_file">
+                                <img src={Logo6} alt='' className='header_file-img'/>
+                                <div className='header_file-info'>
+                                <span className='header_file-name'>File Name</span>
+                                <span className='header_file-descriotion'>File description</span>
+                              
+                                <button><span className='header_file-descriotion'>
+                                <img src={Logo3}  alt='' className='header_file-img1'/>
+                                <a href={pdf} download='pdf'><span className='header_file-descriotion'>
+                                <img src={Logo4} alt='' className='header_file-img1'/>
+                                </span></a>
+                                </span>
+                                </button>
+                              
+                                </div>
+                                
+                        
+                            </p>
+                            
+
                             
     
-                            </div>
+                            
                         </div>
                     </div>
                 </div>  
